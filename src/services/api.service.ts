f5766dacca781781e1d87796c6c0745913bba55f
Etienne Schmitz <hello@etienne-schmitz.com>
2021-02-02 16:16:28 +0100

    fix: fix last warning

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -39,6 +39,7 @@ export default class APIGateway extends Service {
     })
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   public registerDependencies(broker: ServiceBroker): void {
     plugins.forEach((plugin) => this.plugins.push(plugin))
     endpoints.forEach((endpoint) => manager.register(endpoint))
